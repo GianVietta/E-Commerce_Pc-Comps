e0d39fbb372fe73fa33ef6afa81bd7a08f456caa
Barrientos Alex <aalexjuliaan@gmail.com>
2024-11-12 16:44:04 -0300

    Cambios Header
    Se le agrego un menú desplegable y el funcionamiento dependiendo si es usuario administrador o normal

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,33 +1,78 @@
 import { Component, inject, ViewEncapsulation } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink, RouterModule } from '@angular/router';
 import { LoginComponent } from '../../auth/component/login/login.component';
+import { AuthService } from '../../auth/service/auth.service';
+import { User } from '../../auth/interface/auth';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
   encapsulation: ViewEncapsulation.Emulated
 })
 export class HeaderComponent {
   readonly dialog = inject(MatDialog);
+  authService = inject(AuthService);
+  router = inject(Router);
+  
+  isAuthenticated = false;
+  isAdminUser = false;
+  isMenuOpen = false;  // Controla la visibilidad del menú desplegable
 
-  login(){
-    const dialogRef = this.dialog.open(LoginComponent, {
-      disableClose : true ,
-      autoFocus: true,
-      closeOnNavigation : false,
-      position: {top: '50px'},
-      width : '1000px',
-      data : {
-        tipo: 'LOGIN'
-      }
+  user: User | undefined = this.authService.currentUser;
+
+  get getUser(): User | undefined {
+    return this.authService.currentUser;
+  }
+
+  ngOnInit() {
+    this.authService.authStatusChanges().subscribe(isAuthenticated => {
+      this.isAuthenticated = isAuthenticated;
     });
-    
+  }
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen; // Cambiar estado del menú
+  }
+
+  viewProfile() {
+    this.router.navigate(['/perfil']); // Redirige a la página de perfil
+    this.isMenuOpen = false;  // Cierra el menú después de hacer clic
+  }
+
+  logout() {
+    this.authService.logout();
+    this.isAuthenticated = false;
+    this.isAdminUser = false;
+    this.isMenuOpen = false;  // Cierra el menú después de hacer clic
+  }
+
+  login() {
+    const dialogRef = this.dialog.open(LoginComponent, {
+      disableClose: true,
+      autoFocus: true,
+      closeOnNavigation: false,
+      position: { top: '50px' },
+      width: '1000px',
+      data: { tipo: 'LOGIN' }
+    });
+
     dialogRef.afterClosed().subscribe(result => {
-      console.log(`Dialog result: ${result}`);
+      this.authService.checkStatusAutentication().subscribe(
+        auth => {
+          this.isAuthenticated = auth;
+      // Revisa el ID para determinar si es un administrador
+      if (this.getUser?.id === '1') {
+        this.isAdminUser = true;
+      } else {
+        this.isAdminUser = false;
+      }
+        }
+      );
     });
   }
 }
